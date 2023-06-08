6fdc28b24fd3a3b80355950ca42f651b8744c298
LiloFr <lilo.frich@gmail.com>
2023-06-08 10:59:35 +0300

    headers csv

--- coinmarket parse/main.py
+++ coinmarket parse/main.py
@@ -3,17 +3,39 @@ from bs4 import BeautifulSoup
 import csv
 import json
 
-url = "https://coinmarketcap.com/all/views/all/"
+# url = "https://coinmarketcap.com/all/views/all/"
+#
+# headers = {
+#     "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) "
+#                   "Chrome/105.0.0.0 Mobile Safari/537.36 ",
+#     "accept": "application/json, text/plain, */*"
+# }
+#
+# req = requests.get(url, headers=headers)
+#
+# src = req.text
+#
+# with open("index.html", "w", encoding="utf-8") as file:
+#     file.write(src)
 
-headers = {
-    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/105.0.0.0 Mobile Safari/537.36 ",
-    "accept": "application/json, text/plain, */*"
-}
+with open("index.html", "r", encoding="utf-8") as file:
+    src = file.read()
 
-req = requests.get(url, headers=headers)
+soup = BeautifulSoup(src, "lxml")
 
-src = req.text
+table_headers = soup.find(class_="cmc-table__table-wrapper-outer").find("thead").find("tr").find_all("th")
 
-with open("index.html", "w", encoding="utf-8") as file:
-    file.write(src)
+csv_headers = []
+
+for item in table_headers:
+    csv_headers.append(item.text)
+
+with open("currency.csv", "w", encoding="utf-8-sig") as file:
+    writer = csv.writer(file, delimiter=";")
+    writer.writerow(
+        (
+            csv_headers
+        )
+    )
+
+print(csv_headers)
